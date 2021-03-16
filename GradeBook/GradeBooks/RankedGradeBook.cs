07aa7f113f7c5557abd6bba388e3bdcc8fc90f86
dirceu junior <dirceu.sjr@gmail.com>
2021-03-15 21:31:07 -0300

    RankedGradeBook's GetLetterGrade Overridden

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,6 +1,7 @@
 ﻿using GradeBook.Enums;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace GradeBook.GradeBooks
@@ -10,6 +11,61 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base (name)
         {
             Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
+            }
+
+            // separando a lista de médias dos estudantes
+            var avgGrades = new List<double>();
+            foreach (var student in Students)
+                avgGrades.Add(student.AverageGrade);
+
+            // calcular percentis
+            var percentiles = CalculatePercentile(20, avgGrades);                    
+
+            // atribuir letra a nota
+            switch(averageGrade)
+            {
+                case var avg when avg >= percentiles[3]:
+                    return 'A';
+                case var avg when avg >= percentiles[2]:
+                    return 'B';
+                case var avg when avg >= percentiles[1]:
+                    return 'C';
+                case var avg when avg >= percentiles[0]:
+                    return 'D';
+                default:
+                    return 'F';
+            }
+
+            return 'a';
+        }
+
+        public double[] CalculatePercentile(int tamanhoPercentil, IEnumerable<double> valores)
+        {
+            var valoresOrdenados = valores.OrderBy(v => v).ToArray();
+
+            var iteradorMaximo = 100 / tamanhoPercentil;
+
+            var listaPercentis = new List<double>();
+
+            for (var i = 1; i < iteradorMaximo; i++)
+            {
+                var posicaoPercentil = i * ((double)tamanhoPercentil / 100) * valoresOrdenados.Length;
+
+                if (posicaoPercentil % 1 == 0) // verifica se a posição é um inteiro e faz a média de x(i) e x(i+1)
+                    listaPercentis.Add((valoresOrdenados[(int)posicaoPercentil - 1] + valoresOrdenados[(int)posicaoPercentil]) / 2);
+                else // se for decimal, arredonda pra baixo
+                    listaPercentis.Add(valoresOrdenados[(int)Math.Floor(posicaoPercentil)]);
+            }
+
+            return listaPercentis.ToArray();
+
         }
     }
 }
---
+++ GradeBookTests/MyTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using GradeBook.GradeBooks;
+using FluentAssertions;
+
+namespace GradeBookTests
+{
+    public class MyTests
+    {
+
+        [Fact]
+        public void RankedPercentil()
+        {
+            // Arrange
+            var gradeBookRankeado = new RankedGradeBook("teste");
+            var grades = new List<double> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            var tamanhoPercentil = 20;
+            var percentisEsperados = new double[] {2.5,4.5,7,8.5 };
+
+            // Act
+            var result = gradeBookRankeado.CalculatePercentile(tamanhoPercentil, grades);
+
+            // Assert
+            result.Should().BeEquivalentTo(percentisEsperados);
+        }
+    }
+}
